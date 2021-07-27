0f817082b4cf181218616ad7e9ac560c867f9257
Carson Boden <cwboden@umich.edu>
2021-07-26 22:39:46 -0600

    Install Zsh, Vim, and Symlinks via Bootstrap (#3)
    * Implement and test FileExistsBuildPredicate
    
    * Implement and test MakeDirectoryBuildAction
    
    * Install precommit, prepare to use bootstrap to do the same
    
    * Run Black linter and reorder-imports on build files
    
    * Implement and test RunShellCommandBuildAction
    
    * Implement and test Builder
    
    * Add Python .gitignore
    
    * Create first Bootstrap build unit with integration test
    
    * Substitute subprocess.run for check_call to ensure commands exit successfully
    
    * Ensure pip installs pre-commit before trying to run pre-commit
    
    * Try running Git Hooks instead of checking for a file
    
    * Run pre-commit hooks on all files
    
    * Run pre-commit hooks on all files
    
    * Create Vim folders using TDD'd MakeDirectoryBuildUnit
    
    * Add a BuildUnit to ensure VimPlug is installed
    
    * Install VimPlug plugins automatically
    
    * Extract building common dependencies into a helper function
    
    * Extract Vim setup into a helper function
    
    * Download and run oh-my-zsh installer
    
    * Install Powerlevel10k Zsh theme
    
    * Implement and test crawl_for_symlink_sources
    
    * Implement and test translate_symlink_to_destination
    
    * Implement and test create_symlinks
    
    * Add an integration test to ensure symlinks are created correctly
    
    * Update create_symlinks to use the BuildUnit pattern
    
    * Install Zsh (through Debian based distros)
    
    * Install Zsh using GitHub Workflows instead of via bootstrap
    
    * Try installing VimPlug using vim's -c flag
    
    * Install Vim after dotfiles have been linked so that the correct .vimrc is used
    
    * Use the abspath instead of relative path when creating symlinks
    
    * Add an output directory to install_zsh()
    
    * Implement and test PythonModuleExistsBuildPredicate
    
    * Check if pre-commit is installed before trying to install it.
    
    * Create new util module for Python utilities
    
    * Create a TemporaryFilesystem object that cleans itself up
    
    * Implement and test more TemporaryFilesystem functions.
    
    * Avoid freezing the terminal when gruvbox is not yet installed.
    
    * Also install types-setuptool for mypy use within the GitHub Workflow
    
    * Use mypy to install stubs for missing dependencies
    
    * Install mypy
    
    * Give the current directory to mypy to add types
    
    * Use deeper import statements to make mypy happy
    
    * Add this repo to the :/home/cwboden/.dotfiles
    
    * Source dotfiles during Workflow
    
    * XXX: Test to see what $PYTHONPATH is
    
    * Remove the test and ensure this directory is also added to the path
    
    * Remove sourcing dotfiles (not used)
    
    * Explain why we export to the PYTHONPATH
    
    * Install pkg_resources when bootstrapping
    
    * Remove the dependency on pkg_resoureces

--- build/bootstrap_test.py
+++ build/bootstrap_test.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
 import os
+import shutil
 import subprocess
 import unittest
+from pathlib import Path
 
-from bootstrap import AlwaysTrueBuildPredicate
-from bootstrap import BuildAction
-from bootstrap import Builder
-from bootstrap import BuildPredicate
-from bootstrap import BuildUnit
-from bootstrap import FileExistsBuildPredicate
-from bootstrap import MakeDirectoryBuildAction
-from bootstrap import RunShellCommandBuildAction
+from build.bootstrap import AlwaysRunBuildPredicate
+from build.bootstrap import BuildAction
+from build.bootstrap import Builder
+from build.bootstrap import BuildPredicate
+from build.bootstrap import BuildUnit
+from build.bootstrap import crawl_for_symlink_sources
+from build.bootstrap import create_symlinks
+from build.bootstrap import DirectoryExistsBuildPredicate
+from build.bootstrap import FileExistsBuildPredicate
+from build.bootstrap import MakeDirectoryBuildAction
+from build.bootstrap import PythonModuleInstalledBuildPredicate
+from build.bootstrap import RunShellCommandBuildAction
+from build.bootstrap import translate_symlink_to_destination
 
 
 class FileExistsBuildPredicateTest(unittest.TestCase):
@@ -26,6 +33,31 @@ class FileExistsBuildPredicateTest(unittest.TestCase):
         open(self.path, "w").close()
         self.assertTrue(predicate.check())
         os.remove(self.path)
+
+
+class DirectoryExistsBuildPredicateTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.path = "foobar/"
+
+    def test_false_if_file_does_not_exist(self) -> None:
+        predicate = DirectoryExistsBuildPredicate(self.path)
+        self.assertFalse(predicate.check())
+
+    def test_true_if_file_exists(self) -> None:
+        predicate = DirectoryExistsBuildPredicate(self.path)
+        os.mkdir(self.path)
+        self.assertTrue(predicate.check())
+        os.rmdir(self.path)
+
+
+class PythonModuleInstalledBuildPredicateTest(unittest.TestCase):
+    def test_false_if_module_not_installed(self) -> None:
+        predicate = PythonModuleInstalledBuildPredicate("foobar")
+        self.assertFalse(predicate.check())
+
+    def test_true_if_module_installed(self) -> None:
+        predicate = PythonModuleInstalledBuildPredicate("pre-commit")
+        self.assertTrue(predicate.check())
 
 
 class MakeDirectoryBuildActionTest(unittest.TestCase):
@@ -84,7 +116,7 @@ class SpyBuildActionTest(unittest.TestCase):
 class BuildUnitTest(unittest.TestCase):
     def test_action_triggers_if_conditional_is_true(self) -> None:
         spy_action = SpyBuildAction()
-        unit = BuildUnit(AlwaysTrueBuildPredicate(), spy_action)
+        unit = BuildUnit(AlwaysRunBuildPredicate(), spy_action)
 
         unit.build()
 
@@ -101,7 +133,7 @@ class BuilderTest(unittest.TestCase):
             spy_actions.append(spy_action)
             builder.add_unit(
                 BuildUnit(
-                    AlwaysTrueBuildPredicate(),
+                    AlwaysRunBuildPredicate(),
                     spy_action,
                 ),
             )
@@ -112,14 +144,89 @@ class BuilderTest(unittest.TestCase):
             spy_action.assert_called()
 
 
+class SymlinkDotFilesTest(unittest.TestCase):
+    def test_crawl_for_symlink_sources(self) -> None:
+        os.mkdir("foo")
+        Path("foo/bar.symlink").touch()
+        Path("foo/baz.txt").touch()
+        Path("foo/bat.wav").touch()
+
+        os.mkdir("foo/fah")
+        Path("foo/fah/bir.symlink").touch()
+
+        actual_sources = crawl_for_symlink_sources("foo")
+        self.assertEqual(
+            [
+                "foo/bar.symlink",
+                "foo/fah/bir.symlink",
+            ],
+            actual_sources,
+        )
+
+        shutil.rmtree("foo")
+
+    def test_translate_symlink_to_destination(self) -> None:
+        dest_dir = "/root"
+        self.assertEqual(
+            translate_symlink_to_destination("/home/user/file.symlink", dest_dir),
+            f"{dest_dir}/.file",
+        )
+
+    def test_create_symlinks(self) -> None:
+        os.mkdir("fiz")
+        Path("fiz/bar.symlink").touch()
+        os.mkdir("fiz/fah")
+        Path("fiz/fah/bir.symlink").touch()
+
+        dest_dir = "test_home"
+        os.mkdir(dest_dir)
+
+        builder = Builder()
+        create_symlinks(builder, "fiz", dest_dir)
+        builder.build()
+
+        for root, dirs, files in os.walk(dest_dir):
+            self.assertIn(".bar", files)
+            self.assertIn(".bir", files)
+
+        shutil.rmtree("fiz")
+        shutil.rmtree(dest_dir)
+
+
 class BootstrapIntegrationTest(unittest.TestCase):
     """
     Assumes Bootstrap has been run on the system. Will check to ensure certain
     dependencies are available.
     """
 
+    def setUp(self) -> None:
+        self.home_dir = os.path.expanduser("~")
+
     def test_pre_commit_git_hook_installed(self) -> None:
         subprocess.check_call(["pre-commit", "run", "--all-files"])
+
+    def test_vim_folders_are_created(self) -> None:
+        self.assertTrue(os.path.isdir(f"{self.home_dir}/.vim/swapfiles"))
+        self.assertTrue(os.path.isdir(f"{self.home_dir}/.vim/backups"))
+        self.assertTrue(os.path.isdir(f"{self.home_dir}/.vim/undodir"))
+
+    def test_vim_plug_installed(self) -> None:
+        self.assertTrue(os.path.exists(f"{self.home_dir}/.vim/autoload/plug.vim"))
+        self.assertTrue(os.path.exists(f"{self.home_dir}/.vim/plugged"))
+
+    def test_zsh_installed(self) -> None:
+        self.assertTrue(os.path.exists("/tmp/zsh_installer.sh"))
+        self.assertTrue(os.path.isdir(f"{self.home_dir}/.oh-my-zsh"))
+        self.assertTrue(
+            os.path.isdir(f"{self.home_dir}/.oh-my-zsh/custom/themes/powerlevel10k")
+        )
+
+    def test_symlinks_created(self) -> None:
+        self.assertTrue(os.path.exists(f"{self.home_dir}/.hgrc"))
+        self.assertTrue(os.path.exists(f"{self.home_dir}/.vimrc"))
+        self.assertTrue(os.path.exists(f"{self.home_dir}/.zshrc"))
+        self.assertTrue(os.path.exists(f"{self.home_dir}/.gitconfig"))
+        self.assertTrue(os.path.exists(f"{self.home_dir}/.tmux.conf"))
 
 
 if __name__ == "__main__":
