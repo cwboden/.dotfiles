672b8242ec7ce3169f67022106f56a526483b7f9
Carson Boden <cwboden@umich.edu>
2021-07-26 22:39:46 -0600

    Install Zsh, Vim, and Symlinks via Bootstrap (#3)
    * Implement and test FileExistsBuildPredicate
    
    * Implement and test MakeDirectoryBuildAction
    
    * Install precommit, prepare to use bootstrap to do the same
    
    * Run Black linter and reorder-imports on build files
    
    * Implement and test RunShellCommandBuildAction
    
    * Implement and test Builder
    
    * Add Python .gitignore
    
    * Create first Bootstrap build unit with integration test
    
    * Substitute subprocess.run for check_call to ensure commands exit successfully
    
    * Ensure pip installs pre-commit before trying to run pre-commit
    
    * Try running Git Hooks instead of checking for a file
    
    * Run pre-commit hooks on all files
    
    * Run pre-commit hooks on all files
    
    * Create Vim folders using TDD'd MakeDirectoryBuildUnit
    
    * Add a BuildUnit to ensure VimPlug is installed
    
    * Install VimPlug plugins automatically
    
    * Extract building common dependencies into a helper function
    
    * Extract Vim setup into a helper function
    
    * Download and run oh-my-zsh installer
    
    * Install Powerlevel10k Zsh theme
    
    * Implement and test crawl_for_symlink_sources
    
    * Implement and test translate_symlink_to_destination
    
    * Implement and test create_symlinks
    
    * Add an integration test to ensure symlinks are created correctly
    
    * Update create_symlinks to use the BuildUnit pattern
    
    * Install Zsh (through Debian based distros)
    
    * Install Zsh using GitHub Workflows instead of via bootstrap
    
    * Try installing VimPlug using vim's -c flag
    
    * Install Vim after dotfiles have been linked so that the correct .vimrc is used
    
    * Use the abspath instead of relative path when creating symlinks
    
    * Add an output directory to install_zsh()
    
    * Implement and test PythonModuleExistsBuildPredicate
    
    * Check if pre-commit is installed before trying to install it.
    
    * Create new util module for Python utilities
    
    * Create a TemporaryFilesystem object that cleans itself up
    
    * Implement and test more TemporaryFilesystem functions.
    
    * Avoid freezing the terminal when gruvbox is not yet installed.
    
    * Also install types-setuptool for mypy use within the GitHub Workflow
    
    * Use mypy to install stubs for missing dependencies
    
    * Install mypy
    
    * Give the current directory to mypy to add types
    
    * Use deeper import statements to make mypy happy
    
    * Add this repo to the :/home/cwboden/.dotfiles
    
    * Source dotfiles during Workflow
    
    * XXX: Test to see what $PYTHONPATH is
    
    * Remove the test and ensure this directory is also added to the path
    
    * Remove sourcing dotfiles (not used)
    
    * Explain why we export to the PYTHONPATH
    
    * Install pkg_resources when bootstrapping
    
    * Remove the dependency on pkg_resoureces

--- build/bootstrap.py
+++ build/bootstrap.py
@@ -13,9 +13,9 @@ class BuildPredicate(Protocol):
         return False
 
 
-class AlwaysTrueBuildPredicate(BuildPredicate):
+class AlwaysRunBuildPredicate(BuildPredicate):
     def check(self) -> bool:
-        return True
+        return False
 
 
 class FileExistsBuildPredicate(BuildPredicate):
@@ -26,6 +26,27 @@ class FileExistsBuildPredicate(BuildPredicate):
 
     def check(self) -> bool:
         return os.path.exists(self.path)
+
+
+class DirectoryExistsBuildPredicate(BuildPredicate):
+    """Checks whether the given directory exists"""
+
+    def __init__(self, path: str):
+        self.path = path
+
+    def check(self) -> bool:
+        return os.path.isdir(self.path)
+
+
+class PythonModuleInstalledBuildPredicate(BuildPredicate):
+    """Checks whether a provided Python module is installed"""
+
+    def __init__(self, module: str):
+        self.module = module
+
+    def check(self) -> bool:
+        installed_packages = subprocess.check_output(["pip", "list"]).decode("utf-8")
+        return self.module in installed_packages
 
 
 class BuildAction(Protocol):
@@ -55,14 +76,41 @@ class RunShellCommandBuildAction(BuildAction):
         subprocess.check_call(self.command)
 
 
+class MakeSymlinkBuildAction(BuildAction):
+    """Creates a link from source to destination"""
+
+    def __init__(self, source_path: str, dest_path: str):
+        self.source_path = source_path
+        self.dest_path = dest_path
+
+    def execute(self) -> None:
+        os.symlink(self.source_path, self.dest_path)
+
+
 class BuildUnit:
     def __init__(self, predicate: BuildPredicate, action: BuildAction):
         self.predicate = predicate
         self.action = action
 
     def build(self) -> None:
-        if self.predicate.check():
+        if not self.predicate.check():
             self.action.execute()
+
+
+class MakeDirectoryBuildUnit(BuildUnit):
+    """Creates the given directory if it does not exist"""
+
+    def __init__(self, path: str):
+        self.predicate = DirectoryExistsBuildPredicate(path)
+        self.action = MakeDirectoryBuildAction(path)
+
+
+class InstallPythonModuleBuildUnit(BuildUnit):
+    """Installs a Python module if it isn't yet installed"""
+
+    def __init__(self, module: str):
+        self.predicate = PythonModuleInstalledBuildPredicate(module)
+        self.action = RunShellCommandBuildAction(["pip", "install", module])
 
 
 class Builder:
@@ -77,14 +125,22 @@ class Builder:
             unit.build()
 
 
-def main() -> None:
-    builder = Builder()
-
+def install_common_dependencies(builder: Builder) -> None:
     # Install Python dependencies
+    for module in [
+        "mypy",
+        "pre-commit",
+        "types-setuptools",
+    ]:
+        builder.add_unit(InstallPythonModuleBuildUnit(module))
+
+    # Have mypy install type stubs for any external libraries
     builder.add_unit(
         BuildUnit(
-            AlwaysTrueBuildPredicate(),
-            RunShellCommandBuildAction(["pip", "install", "pre-commit"]),
+            AlwaysRunBuildPredicate(),
+            RunShellCommandBuildAction(
+                ["python", "-m", "mypy", "--install-types", "./"]
+            ),
         ),
     )
 
@@ -95,6 +151,155 @@ def main() -> None:
             RunShellCommandBuildAction(["pre-commit", "install"]),
         ),
     )
+
+
+def install_vim(builder: Builder) -> None:
+    # Create Vim folders
+    home_dir = os.path.expanduser("~")
+    builder.add_unit(MakeDirectoryBuildUnit(f"{home_dir}/.vim/"))
+    for folder in ["swapfiles", "backups", "undodir"]:
+        builder.add_unit(MakeDirectoryBuildUnit(f"{home_dir}/.vim/{folder}"))
+
+    # Install VimPlug
+    builder.add_unit(
+        BuildUnit(
+            FileExistsBuildPredicate(f"{home_dir}/.vim/autoload/plug.vim"),
+            RunShellCommandBuildAction(
+                [
+                    "curl",
+                    "-fLo",
+                    f"{home_dir}/.vim/autoload/plug.vim",
+                    "--create-dirs",
+                    "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim",
+                ]
+            ),
+        ),
+    )
+    builder.add_unit(
+        BuildUnit(
+            DirectoryExistsBuildPredicate(f"{home_dir}/.vim/plugged"),
+            RunShellCommandBuildAction(
+                [
+                    "vim",
+                    "+PlugInstall",
+                    "+qa",
+                ]
+            ),
+        ),
+    )
+
+
+def install_zsh(builder: Builder, home_dir: str) -> None:
+    zsh_installer_path = "/tmp/zsh_installer.sh"
+
+    # Download installer
+    builder.add_unit(
+        BuildUnit(
+            FileExistsBuildPredicate(zsh_installer_path),
+            RunShellCommandBuildAction(
+                [
+                    "wget",
+                    "-O",
+                    zsh_installer_path,
+                    "https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh",
+                ]
+            ),
+        ),
+    )
+
+    # Run installer
+    builder.add_unit(
+        BuildUnit(
+            DirectoryExistsBuildPredicate(f"{home_dir}/.oh-my-zsh"),
+            RunShellCommandBuildAction(
+                [
+                    "sh",
+                    zsh_installer_path,
+                    "--unattended",
+                    "--keep-zshrc",
+                ]
+            ),
+        ),
+    )
+
+    # Install powerlevel10k theme
+    builder.add_unit(
+        BuildUnit(
+            DirectoryExistsBuildPredicate(
+                f"{home_dir}/.oh-my-zsh/custom/themes/powerlevel10k"
+            ),
+            RunShellCommandBuildAction(
+                [
+                    "git",
+                    "clone",
+                    "--depth=1",
+                    "https://github.com/romkatv/powerlevel10k.git",
+                    f"{home_dir}/.oh-my-zsh/custom/themes/powerlevel10k",
+                ]
+            ),
+        ),
+    )
+
+
+def crawl_for_symlink_sources(start_directory: str) -> List[str]:
+    """
+    Crawls through the start directory and all subdirectories for files that
+    end with `.symlink`
+    """
+
+    sources = set()
+    for root, dirs, files in os.walk(start_directory):
+        for name in files:
+            if name.endswith(".symlink"):
+                sources.add(os.path.join(root, name))
+
+    sorted_order = list(sources)
+    sorted_order.sort()
+    return sorted_order
+
+
+def translate_symlink_to_destination(symlink: str, destination: str) -> str:
+    """
+    Finds the translation from a *.symlink file to where its symlink should
+    live in the home directory. This follows a straightforward pattern:
+      1. The `.symlink` suffix will be removed
+      2. It will start with a `.`
+    """
+
+    file_name = symlink.split("/")[-1].replace(".symlink", "")
+    return f"{destination}/.{file_name}"
+
+
+def create_symlinks(builder: Builder, source_dir: str, dest_dir: str) -> None:
+    """
+    Crawl through the source_dir for any files that end in ".symlink" and
+    create symlinks to them in the dest_dir
+    """
+
+    sources = crawl_for_symlink_sources(source_dir)
+    destinations = [
+        translate_symlink_to_destination(source, dest_dir) for source in sources
+    ]
+
+    for source, destination in zip(sources, destinations):
+        builder.add_unit(
+            BuildUnit(
+                FileExistsBuildPredicate(destination),
+                MakeSymlinkBuildAction(os.path.abspath(source), destination),
+            ),
+        )
+
+
+def main() -> None:
+    builder = Builder()
+
+    install_common_dependencies(builder)
+
+    home_dir = os.path.expanduser("~")
+    install_zsh(builder, home_dir)
+    create_symlinks(builder, "./", home_dir)
+
+    install_vim(builder)
 
     builder.build()
 
