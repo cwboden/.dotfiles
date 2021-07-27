262b7a3b1d3b0eba0db39106af0190903a758fac
Carson Boden <cwboden@umich.edu>
2021-07-26 22:18:21 -0700

    Add additional git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -3,4 +3,4 @@ alias gac='git add -A && git commit -m'
 alias gpom="git push origin main"
 alias gfom="git pull origin main"
 alias gcom="git commit -m"
-alias gs="git status"
+alias gb='git branch'
