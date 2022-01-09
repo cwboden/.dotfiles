5849c488ef832b8f3cb1f49347f0925d6b6d2350
Carson Boden <carson.boden@gmail.com>
2022-01-08 22:59:06 -0800

    feat(path-finding): Add argument parsing for output format.

--- examples/path_finding.rs
+++ examples/path_finding.rs
@@ -3,34 +3,57 @@ use assert_matches::assert_matches;
 #[derive(Debug, Eq, PartialEq)]
 enum Error {
     InvalidArgument(String),
+    DuplicateArgument(String),
 }
 
 type Result<T> = std::result::Result<T, Error>;
 
 #[derive(Debug, Eq, PartialEq)]
-enum Mode {
+enum Algorithm {
     Queue,
     Stack,
 }
 
 #[derive(Debug, Eq, PartialEq)]
+enum Format {
+    List,
+    Map,
+}
+
+#[derive(Debug, Eq, PartialEq)]
 struct PathFindingArgs {
-    mode: Option<Mode>,
+    algorithm: Option<Algorithm>,
+    output_format: Option<Format>,
 }
 
 impl PathFindingArgs {
     fn new() -> Self {
-        Self { mode: None }
+        Self {
+            algorithm: None,
+            output_format: None,
+        }
     }
 
-    fn add_mode(&mut self, mode: Mode) -> Result<()> {
-        match self.mode {
+    fn add_algorithm(&mut self, algorithm: Algorithm) -> Result<()> {
+        match self.algorithm {
             None => {
-                self.mode = Some(mode);
+                self.algorithm = Some(algorithm);
                 Ok(())
             }
-            Some(_) => Err(Error::InvalidArgument(
-                "Cannot specify more than one mode".to_string(),
+            Some(_) => Err(Error::DuplicateArgument(
+                "Cannot specify more than one algorithm mode".to_string(),
+            )),
+        }
+    }
+
+    fn add_output_format(&mut self, output_format: Format) -> Result<()> {
+        match self.output_format {
+            None => {
+                self.output_format = Some(output_format);
+                Ok(())
+            }
+            Some(_) => Err(Error::DuplicateArgument(
+                "Cannot specify more than one output format mode".to_string(),
             )),
         }
     }
@@ -40,12 +63,25 @@ fn parse_args(args: &[String]) -> Result<PathFindingArgs> {
     let mut parsed_args = PathFindingArgs::new();
 
     for arg in args {
-        if arg == "-q" || arg == "--queue" {
-            parsed_args.add_mode(Mode::Queue)?;
-        } else if arg == "-s" || arg == "--stack" {
-            parsed_args.add_mode(Mode::Stack)?;
-        } else {
-            return Err(Error::InvalidArgument(format!("Invalid argument: '{}'", arg)));
+        match arg.as_str() {
+            "-q" | "--queue" => {
+                parsed_args.add_algorithm(Algorithm::Queue)?;
+            }
+            "-s" | "--stack" => {
+                parsed_args.add_algorithm(Algorithm::Stack)?;
+            }
+            "-l" | "--list" => {
+                parsed_args.add_output_format(Format::List)?;
+            }
+            "-m" | "--map" => {
+                parsed_args.add_output_format(Format::Map)?;
+            }
+            _ => {
+                return Err(Error::InvalidArgument(format!(
+                    "Invalid argument: '{}'",
+                    arg
+                )))
+            }
         }
     }
 
@@ -61,26 +97,66 @@ mod tests {
     use super::*;
 
     #[test]
-    fn parse_args_mode_queue() {
+    fn parse_args_algorithm_queue() {
         assert_eq!(
-            parse_args(&["-q".to_string()]).unwrap().mode,
-            Some(Mode::Queue)
+            parse_args(&["-q".to_string()]).unwrap().algorithm,
+            Some(Algorithm::Queue)
         );
         assert_eq!(
-            parse_args(&["--queue".to_string()]).unwrap().mode,
-            Some(Mode::Queue),
+            parse_args(&["--queue".to_string()]).unwrap().algorithm,
+            Some(Algorithm::Queue),
         );
     }
 
     #[test]
-    fn parse_args_mode_stack() {
+    fn parse_args_algorithm_stack() {
         assert_eq!(
-            parse_args(&["-s".to_string()]).unwrap().mode,
-            Some(Mode::Stack)
+            parse_args(&["-s".to_string()]).unwrap().algorithm,
+            Some(Algorithm::Stack)
         );
         assert_eq!(
-            parse_args(&["--stack".to_string()]).unwrap().mode,
-            Some(Mode::Stack)
+            parse_args(&["--stack".to_string()]).unwrap().algorithm,
+            Some(Algorithm::Stack)
+        );
+    }
+
+    #[test]
+    fn parse_args_algorithm_duplicate_error() {
+        assert_matches!(
+            parse_args(&["-s".to_string(), "-q".to_string()]),
+            Err(Error::DuplicateArgument(_))
+        );
+    }
+
+    #[test]
+    fn parse_args_output_format_list() {
+        assert_eq!(
+            parse_args(&["-l".to_string()]).unwrap().output_format,
+            Some(Format::List)
+        );
+        assert_eq!(
+            parse_args(&["--list".to_string()]).unwrap().output_format,
+            Some(Format::List)
+        );
+    }
+
+    #[test]
+    fn parse_args_output_format_map() {
+        assert_eq!(
+            parse_args(&["-m".to_string()]).unwrap().output_format,
+            Some(Format::Map)
+        );
+        assert_eq!(
+            parse_args(&["--map".to_string()]).unwrap().output_format,
+            Some(Format::Map)
+        );
+    }
+
+    #[test]
+    fn parse_args_output_format_duplicate_error() {
+        assert_matches!(
+            parse_args(&["-m".to_string(), "-l".to_string()]),
+            Err(Error::DuplicateArgument(_))
         );
     }
 
