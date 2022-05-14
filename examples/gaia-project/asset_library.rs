7cbda0f94e2c3cc7640c836132809e82dad30581
Carson Boden <carson.boden@gmail.com>
2022-05-14 15:46:16 -0700

    feat(planets): load planet sprites.
    TODO: Build out a more general purpose way to load assets.

--- examples/gaia-project/asset_library.rs
+++ examples/gaia-project/asset_library.rs
@@ -7,6 +7,7 @@ use crate::GameState;
 #[derive(Default)]
 pub struct AssetLibrary {
     fonts: HashMap<String, Handle<Font>>,
+    sprites: HashMap<String, Handle<Sprite>>,
 }
 
 impl AssetLibrary {
@@ -17,6 +18,17 @@ impl AssetLibrary {
                 "Could not find font: \'{}\' in font library: {:?}",
                 name,
                 self.fonts.keys().collect::<Vec<&String>>()
+            ),
+        }
+    }
+
+    pub fn sprite(&self, name: &str) -> Handle<Sprite> {
+        match self.sprites.get(name) {
+            Some(handle) => handle.clone(),
+            None => panic!(
+                "Could not find sprite: \'{}\' in sprite library: {:?}",
+                name,
+                self.sprites.keys().collect::<Vec<&String>>()
             ),
         }
     }
@@ -36,14 +48,33 @@ fn init_assets(
     mut asset_library: ResMut<AssetLibrary>,
     asset_server: Res<AssetServer>,
 ) {
-    let fonts = vec![("game", "fonts/MartianMono-StdRg.ttf")];
-
-    for &(title, file_location) in fonts.iter() {
-        asset_library
-            .fonts
-            .insert(title.into(), asset_server.load(file_location));
-    }
-
+    [("game", "fonts/MartianMono-StdRg.ttf")]
+        .iter()
+        .for_each(|&(title, file_location)| {
+            asset_library
+                .fonts
+                .insert(title.into(), asset_server.load(file_location));
+        });
     println!("Loaded fonts: {:?}", asset_library.fonts);
+
+    [
+        ("planet-red", "sprites/planet-red.png"),
+        ("planet-yellow", "sprites/planet-yellow.png"),
+        ("planet-orange", "sprites/planet-orange.png"),
+        ("planet-grey", "sprites/planet-grey.png"),
+        ("planet-white", "sprites/planet-white.png"),
+        ("planet-brown", "sprites/planet-brown.png"),
+        ("planet-blue", "sprites/planet-blue.png"),
+        ("planet-gaia", "sprites/planet-gaia.png"),
+        ("planet-transdim", "sprites/planet-transdim.png"),
+    ]
+    .iter()
+    .for_each(|&(title, file_location)| {
+        asset_library
+            .sprites
+            .insert(title.into(), asset_server.load(file_location));
+    });
+    println!("Loaded sprites: {:?}", asset_library.sprites);
+
     game_state.set(GameState::Running).unwrap();
 }
