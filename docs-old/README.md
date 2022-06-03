fbe6b0ceb2271b8419c04c11cfce994ba13f5d1c
Carson Boden <carson.boden@gmail.com>
2022-06-03 15:10:26 -0700

    Merge new Jekyll site into `main` to test GitHub Pages publishing. (#36)
    * feat(ruby): Install Ruby gems to `~/gems/`.
    
    * refactor: Move `docs/` -> `docs-old/` with old site files.
    
    * feat: Initial GitHub pages site using Jekyll.
    
    * migrate: Make the `docs-old/` folder contain the real old website.
    
    * feat: Remove old yarn-based CI/CD for the website.
    
    This will be re-tooled once Jekyll is setup.

---
+++ docs-old/README.md
@@ -0,0 +1,2 @@
+# My Portfolio Page
+During college, anyways
--- docs/src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import { screen } from "@testing-library/react"
-import { render } from "./test-utils"
-import { App } from "./App"
-
-test("testing works correctly", () => {
-  expect(true).toBeTruthy()
-})
