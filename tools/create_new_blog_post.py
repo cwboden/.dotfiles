a7fd728febd495ab3bb7c69839d982ad0399fc24
Carson Boden <carson.boden@gmail.com>
2022-06-14 22:50:20 -0700

    feat: Create a script for branching and starting a new blog post (#39)
    * feat(create-new-blog-post): Touch the file in valid Jekyll format.
    
    * feat(create-new-blog-post): Reformat title input to use kebab-case
    
    * feat(create-new-blog-post): Avoid creating a new post if one already exists.
    
    * feat(new-blog-post): Copy the template to the newly created blog post.
    
    * dep(GitPython): Add `GitPython` as a dependency for making `git` calls.
    
    * feat(new-blog-post): Checkout a new branch when creating a post.
    
    * feat(new-blog-post): test checking out a new branch
    
    * fix: use `TEMPLATE_PATH` from the parent library instead of importing.
    
    * fix: Find the blog template relative to the script.

---
+++ tools/create_new_blog_post.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+import re
+import shutil
+import sys
+from argparse import ArgumentDefaultsHelpFormatter
+from argparse import ArgumentParser
+from argparse import Namespace
+from datetime import date
+from pathlib import Path
+from typing import List
+
+from git import Repo
+
+TEMPLATE_PATH: Path = Path(__file__).parent.joinpath("blog-post-template.md")
+
+
+def parse_args(args: List[str]) -> Namespace:
+    parser = ArgumentParser(
+        description="Create a new blog post for GitHub Pages in Jekyll format",
+        formatter_class=ArgumentDefaultsHelpFormatter,
+    )
+
+    parser.add_argument("title", type=str, help="the name of the blog post")
+    parser.add_argument(
+        "path",
+        type=Path,
+        nargs="?",
+        default=Path.home().joinpath(".dotfiles/docs/_posts/"),
+        help="where to create the new post",
+    )
+
+    return parser.parse_args(args)
+
+
+def kebab_case(s: str) -> str:
+    # Split "snake_case" apart into "snake case"
+    underscore = re.compile(r"_")
+    s = underscore.sub(" ", s)
+
+    # Remove punctuation
+    punctuation = re.compile(r"[?!.,\'\"]")
+    s = punctuation.sub(" ", s)
+
+    # Shorten multiple spaces into one
+    multispace = re.compile(r"\s+")
+    s = multispace.sub(" ", s)
+
+    spaces = re.compile(r" ")
+    return spaces.sub("-", s.strip()).lower()
+
+
+def main(args: Namespace, repo: Repo) -> Path:
+    today = date.today().strftime("%Y-%m-%d")
+    title = kebab_case(args.title)
+
+    path_to_file = Path(f"{args.path}/{today}-{title}.md")
+
+    if path_to_file.exists():
+        raise Exception(f"Cannot create new post '{path_to_file}'. Post exists.")
+
+    branch_name = f"posts/{today}-{title}"
+    if repo.is_dirty():
+        raise Exception(
+            f"Cannot create git branch: '{branch_name}'. Repo has outstanding changes."
+        )
+
+    repo.heads.main.checkout()
+    repo.create_head(branch_name).checkout()
+
+    shutil.copyfile(TEMPLATE_PATH, path_to_file)
+    return path_to_file
+
+
+if __name__ == "__main__":
+    args = parse_args(sys.argv[1:])
+    repo = Repo(Path.home().joinpath(".dotfiles"))
+    main(args, repo)
