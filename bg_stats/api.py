e52e8538a9f5869fffe833bbc58e6a2ba6b57406
Carson Boden <carson.boden@gmail.com>
2022-07-14 17:27:11 -0700

    feat(bg-stats): Add properties to return players, games, locations, and plays.

--- bg_stats/api.py
+++ bg_stats/api.py
@@ -1,8 +1,96 @@
 import json
 from pathlib import Path
+from typing import Any
+from typing import List
+from typing import TypedDict
+
+
+class Player(TypedDict):
+    uuid: str
+    id: int
+    name: str
+    isAnonymous: bool
+    modificationDate: str
+    bggUsername: str
+
+
+class Location(TypedDict):
+    uuid: str
+    id: int
+    name: str
+    modificationDate: str
+
+
+class Game(TypedDict):
+    uuid: str
+    id: int
+    name: str
+    modificationDate: str
+    cooperative: bool
+    highestWins: bool
+    noPoints: bool
+    usesTeams: bool
+    urlThumb: str
+    urlImage: str
+    bggName: str
+    bggYear: int
+    bggId: int
+    designers: str
+    metadata: str
+    isBaseGame: int
+    isExpansion: int
+    rating: int
+    minPlayerCount: int
+    maxPlayerCount: int
+    minPlayTime: int
+    maxPlayTime: int
+    minAge: int
+    preferredImage: int
+    copies: List[Any]
+
+
+class Play(TypedDict):
+    uuid: str
+    modificationDate: str
+    entryDate: str
+    playDate: str
+    usesTeams: bool
+    durationMin: int
+    ignored: bool
+    manualWinner: bool
+    rounds: int
+    bggId: int
+    bggLastSync: str
+    importPlayId: int
+    locationRefId: int
+    gameRefId: int
+    board: str
+    comments: str
+    rating: int
+    nemestatsId: int
+    scoringSetting: int
+    metaData: str
+    playerScores: List[Any]
+    expansionPlays: List[Any]
 
 
 class BgStats:
     def __init__(self, filename: Path):
         with open(filename) as raw_data:
             self.root = json.load(raw_data)
+
+    @property
+    def players(self) -> List[Player]:
+        return self.root["players"]
+
+    @property
+    def locations(self) -> List[Location]:
+        return self.root["locations"]
+
+    @property
+    def games(self) -> List[Game]:
+        return self.root["games"]
+
+    @property
+    def plays(self) -> List[Play]:
+        return self.root["plays"]
--- bg_stats/api_test.py
+++ bg_stats/api_test.py
@@ -4,10 +4,19 @@ from pathlib import Path
 from bg_stats.api import BgStats
 
 
+PATH_TO_DATA: Path = Path("BGStatsExport.json")
+
+
 class BgStatsTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.stats = BgStats(PATH_TO_DATA)
+
     def test_data_is_loaded(self) -> None:
-        stats = BgStats(Path("BGStatsExport.json"))
-        self.assertIsNotNone(stats.root)
+        self.assertIsNotNone(self.stats.root)
+        self.assertIsNotNone(self.stats.players)
+        self.assertIsNotNone(self.stats.locations)
+        self.assertIsNotNone(self.stats.games)
+        self.assertIsNotNone(self.stats.plays)
 
 
 if __name__ == "__main__":
