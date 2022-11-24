4234552a2d6409d354049cdb4650811c4bb2ad14
Carson Boden <carson.boden@gmail.com>
2022-11-24 08:06:19 -0800

    fix: Use the correct install path for VimPlugged.

--- bootstrap/main_integration_test.py
+++ bootstrap/main_integration_test.py
@@ -26,9 +26,9 @@ class BootstrapIntegrationTest(unittest.TestCase):
 
     def test_vim_plug_installed(self) -> None:
         self.assertTrue(
-            os.path.exists(f"{self.home_dir}/.local/share/site/autoload/plug.vim")
+            os.path.exists(f"{self.home_dir}/.local/share/nvim/site/autoload/plug.vim")
         )
-        self.assertTrue(os.path.exists(f"{self.home_dir}/.local/share/site/plugged"))
+        self.assertTrue(os.path.exists(f"{self.home_dir}/.local/share/nvim/plugged"))
 
     def test_tmux_folders_are_created(self) -> None:
         self.assertTrue(os.path.exists(f"{self.home_dir}/.tmux/"))
