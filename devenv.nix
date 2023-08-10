11152082caa6f68a86c09e5df144a5504a9da23e
Carson Boden <carson.boden@gmail.com>
2023-08-10 01:34:11 -0700

    feat: Initialize devenv CI pipeline.

--- devenv.nix
+++ devenv.nix
@@ -1,9 +1,36 @@
-{ pkgs, ... }:
+{ inputs, pkgs, ... }:
 
 {
+  languages.c.enable = true;
+  languages.cplusplus.enable = true;
+
   languages.python.enable = true;
   languages.python.version = "3.11.3";
 
   languages.rust.enable = true;
-  languages.rust.version = "stable";
+  languages.rust.channel = "stable";
+
+  pre-commit = {
+    hooks = {
+      black = {
+        excludes = [ "^practice/" ];
+        enable = true;
+      };
+      clippy.enable = true;
+      hunspell.enable = true;
+      markdownlint = {
+        enable = true;
+      };
+      mdsh.enable = true;
+      mypy = {
+        enable = true;
+        excludes = [ "^practice/" ];
+      };
+      nixpkgs-fmt.enable = true;
+      rustfmt.enable = true;
+      shellcheck.enable = true;
+      shfmt.enable = true;
+      yamllint.enable = true;
+    };
+  };
 }
