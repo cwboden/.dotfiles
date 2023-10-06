b6f9274c189f28ebb59140e597c9805aee9794fb
cwboden <cwboden@users.noreply.github.com>
2023-10-06 16:15:57 +0000

    jekyll build from Action 19a0d92e03a9b04b29ed247f4f5608fa8293c5c9

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,187 @@
+var store = [{
+        "title": "Qumulo",
+        "excerpt":"Served on a variety of teams helping build out an enterprise-scale filesystem enabling customers to manage and curate petabytes of data. Qumulo is Seattle’s latest unicorn, hitting a $1.2 Billion valuation in 2020. On my first team, I helped design and develop a now-patented max-flow algorithm for storage layout, enabling...","categories": [],
+        "tags": [],
+        "url": "/portfolio/86-qumulo/",
+        "teaser": "/assets/img/portfolio/qumulo-thumbnail.jpg"
+      },{
+        "title": "Ada Volunteering",
+        "excerpt":"Ada Developers Academy is a non-profit, cost-free coding school for women and gender expansive adults. We primarily serve Black, Latine, Indigenous Americans, Native Hawaiian &amp; Pacific Islander, LGBTQIA+, and low-income people. I work as a mentor in the program which creates opportunities for tech professionals to use their skills to...","categories": [],
+        "tags": [],
+        "url": "/portfolio/87-ada-volunteering/",
+        "teaser": "/assets/img/portfolio/ada-thumbnail.png"
+      },{
+        "title": "Ocient Internship",
+        "excerpt":"Contributed to the development of an exabyte-scale distributed database. The internship was divided into two parts on two different teams to get a feel for different parts of the company. On the first team, I worked on designing a testing framework to inject errors into system calls made by the...","categories": [],
+        "tags": [],
+        "url": "/portfolio/88-ocient-internship/",
+        "teaser": "/assets/img/portfolio/ocient-thumbnail.png"
+      },{
+        "title": "Computer Science Education Research",
+        "excerpt":"In this special topics course, we were introduced to many topics in computer education research (CER). We learned about designing our own studies such as multi-institutional multi-national, laboratory, and classroom studies. Research was grounded in theory from learning sciences, educational psychology, and science education. The class included a research proposal...","categories": [],
+        "tags": [],
+        "url": "/portfolio/89-eecs498-cs-education/",
+        "teaser": "/assets/img/portfolio/cs-education-thumbnail.png"
+      },{
+        "title": "Hardware Prefetcher Research",
+        "excerpt":"As part of EECS 573: Advanced Topics in Computer Architecture, my lab partner and I designed a simulator to test a programmable prefetcher. The idea was to create a prefetcher that could be tuned to match specific work loads. For example, prefetching could be more aggressive when it will lead...","categories": [],
+        "tags": [],
+        "url": "/portfolio/90-eecs573-hardware-prefetcher/",
+        "teaser": "/assets/img/portfolio/cpu-2-thumbnail.jpg"
+      },{
+        "title": "Qualcomm Internship",
+        "excerpt":"Designed testing and debugging frameworks for DDR systems. The automation platform, developed primarily in Python and C++, allows for rapid analysis of Android binary dumps, giving users instant feedback on hardware and firmware issues. The tool was also designed for and released to third-parties, including Samsung.  ","categories": [],
+        "tags": [],
+        "url": "/portfolio/91-qualcomm-internship/",
+        "teaser": "/assets/img/portfolio/qualcomm-thumbnail.jpg"
+      },{
+        "title": "Computer Architecture Design",
+        "excerpt":"Prototyped and programmed a superscalar, Alpha64-ISA, out-of-order processor with hardware prefetching, advanced branch prediction, and cache enhancements, including non-blocking accesses and a victim cache. The design process was a time-intensive two months for our five team members, during which we spent time creating modules, unit testing, and integrating them into...","categories": [],
+        "tags": [],
+        "url": "/portfolio/92-eecs470-computer-architecture/",
+        "teaser": "/assets/img/portfolio/cpu-thumbnail.jpg"
+      },{
+        "title": "Student Instructor",
+        "excerpt":"Worked as an assistant to professors in EECS 370: Introduction to Computer Organization and EECS 470: Introduction to Computer Architecture. Standard IA responsibilities include hosting office hours, answering student questions on Piazza, and grading assignments. In EECS 370, I personally managed the course website and calendar and hosted a discussion...","categories": [],
+        "tags": [],
+        "url": "/portfolio/93-student-instructor/",
+        "teaser": "/assets/img/portfolio/classroom-thumbnail.jpg"
+      },{
+        "title": "Embedded Systems: The Smart Shower",
+        "excerpt":"Designed and created a user-detecting smart shower that uses ultrasonic sensors and user input to schedule, preheat, and save water during showers. In addition to the ultrasonic sensors, the final design involved the integration of solenoids, a thermistor, a numberic keypad, two servos attached to a gimbal, a serial display,...","categories": [],
+        "tags": [],
+        "url": "/portfolio/94-eecs373-smart-shower/",
+        "teaser": "/assets/img/portfolio/smart-shower-thumbnail.jpg"
+      },{
+        "title": "Amazon Internship",
+        "excerpt":"Aided in the development of a shadow-testing platform used for internal testing. The internship was divided between advancing the testing system and creating a website to aid users as they onboarded and used the service.  ","categories": [],
+        "tags": [],
+        "url": "/portfolio/95-amazon-internship/",
+        "teaser": "/assets/img/portfolio/amazon-thumbnail.png"
+      },{
+        "title": "IPE Robot Programming",
+        "excerpt":"Attended an abroad program in Germany and designed a series of programs, each designed for the m3pi robot (above) to move with a specific purpose. The first script allowed for manual control via Bluetooth and recorded lap times around a track. The next script automated the track-following process through a...","categories": [],
+        "tags": [],
+        "url": "/portfolio/96-ipe-robot-programming/",
+        "teaser": "/assets/img/portfolio/ipe-robot-programming-thumbnail.jpg"
+      },{
+        "title": "Startup High School",
+        "excerpt":"Flagship project of MPowered to encourage high school students to work on great ideas and bring them to life. In our two day entrepreneurial summit and pitch competition, we challenge high schoolers to put their mind to the test for cash scholarships. Co-directed a team of six individuals to attain...","categories": [],
+        "tags": [],
+        "url": "/portfolio/97-startup-high-school/",
+        "teaser": "/assets/img/portfolio/startup-high-school-thumbnail.jpg"
+      },{
+        "title": "Eta Kappa Nu (HKN)",
+        "excerpt":"The Beta Epsilon chapter of HKN is composed of the top electrical, computer, and computer science engineers at the University of Michigan. I served on the officer corps as treasurer, corporate relations, president, and now advisor. As treasurer, I managed funds during the semester including reimbursements, company invoices, and member...","categories": [],
+        "tags": [],
+        "url": "/portfolio/98-hkn/",
+        "teaser": "/assets/img/portfolio/hkn-logo-thumbnail.png"
+      },{
+        "title": "University of Michigan",
+        "excerpt":"Graduated Summa Cum Laude from the University of Michigan with a Bachelor’s in Computer Engineering.   Notable courses taken include EECS 470: Computer Architecture Design, EECS 482: Operating Systems, EECS 281: Data Structures and Algorithms, and EECS 461: Embedded Control Systems.  ","categories": [],
+        "tags": [],
+        "url": "/portfolio/99-university-of-michigan/",
+        "teaser": "/assets/img/portfolio/michigan-thumbnail.jpg"
+      },{
+        "title": "Easy to use tools for larger codebases",
+        "excerpt":"I’ve recently started using tools to aid the development of the game I’ve been working on, and I believe CMake, GoogleTest, and Git Hooks are not only incredibly powerful, but also easy to setup and use. I have only just started learning them, but I imagine they will be continuously...","categories": ["Programming"],
+        "tags": ["tools","cmake","google-test","git-hooks"],
+        "url": "/programming/2018/07/06/easy-to-use-tools-for-larger-codebases.html",
+        "teaser": null
+      },{
+        "title": "Sailing the Belizean coastline",
+        "excerpt":"This past week I had the pleasure of sailing around Belize with my family. This was the third sailing trip we’ve done together, but this one involved some of the toughest weather and volatile waters. We first arrived in Belize City, where we took a puddle-jumper to Placencia. There, my...","categories": ["Personal"],
+        "tags": ["travel","sailing","belize"],
+        "url": "/personal/2018/07/15/sailing-the-belizean-coastline.html",
+        "teaser": null
+      },{
+        "title": "How Nintendo continues to innovate in the hardware industry",
+        "excerpt":"Nintendo has proved time and time again that they don’t need “next-gen” hardware to do well in the console market. With almost 20 million Nintendo Switch devices sold since its release only a year and a half ago, the Switch proves that it can compete with Xbox and Playstation. But...","categories": ["Video Games"],
+        "tags": ["hardware","nintendo","switch"],
+        "url": "/video%20games/2018/07/29/how-nintendo-continues-to-innovate.html",
+        "teaser": null
+      },{
+        "title": "*The Forgotten City*: An intriguing, philosophical first-release from Modern Storyteller",
+        "excerpt":"Release Date: 28 Jul 2021 Developer: Modern Storyteller Publisher: Dear Villagers Overview First off, let me say that I’ve been enjoying the game. I will try to avoid any major spoilers, though if you consider things like diving into the mechanics or addressing themes of the game spoilers, then go...","categories": ["Video Games"],
+        "tags": ["story","simulation","philosophy"],
+        "url": "/video%20games/2022/06/14/the-forgotten-city.html",
+        "teaser": null
+      },{
+        "title": "*Spiritfarer* uses breathtaking illustrations and satisfying gameplay to reduce grind",
+        "excerpt":"Release Date: 18 Aug 2021 Developer: Thunder Lotus Games Publisher: Thunder Lotus Games Overview Spiritfarer is “a cozy game about dying.” You play as Stella, a young woman tasked with ferrying the dead, which, while it sounds morbid, actually involves a relaxing sail around various islands, completing the cute –...","categories": ["Video Games"],
+        "tags": ["platformer","story","simulation","resource-management"],
+        "url": "/video%20games/2022/06/20/spiritfarer.html",
+        "teaser": null
+      },{
+        "title": "How `git` works *(from a work presentation)*",
+        "excerpt":"There are plenty of guides to using git all around the web. Why should I add another drop to a vast ocean of resources? Well, as someone who’s professional experience with VCS has only involved Mercurial, I wanted to give myself a deeper understand of a tool I was going...","categories": ["Programming"],
+        "tags": ["git","tools","education"],
+        "url": "/programming/2022/07/13/git-basics.html",
+        "teaser": null
+      },{
+        "title": "Quarantine Quick Reviews: Thoughts on the games I played while sick with CoViD.",
+        "excerpt":"It finally happened: I got CoViD. After more than two years of evading the virus, the inevitable is here. Thankfully, I’ve been (mostly) fine – no averse reactions, fairly standard symptoms. If anything, I’m mainly just waiting for when I’ll feel back to 100%. But during my time in relative...","categories": ["Video Games"],
+        "tags": ["co-op","metroidvania","platformer","point-and-click","puzzle","racing","resource-management","shooter","simulation","story"],
+        "url": "/video%20games/2022/07/18/quarantine-quick-reviews.html",
+        "teaser": null
+      },{
+        "title": "An Attempt to Analyze Three Years of Board Game Stats",
+        "excerpt":"Source Code The Spark Three years ago, during a summer in Chicago, I decided that, with all of the board games I’d been playing, it would be fun to track them and see trends, average scores, and time spent playing games. While it’s fun to nerd out about personal statistics,...","categories": ["Board Games","Programming"],
+        "tags": ["statistics","tools","grafana"],
+        "url": "/board%20games/programming/2022/08/01/bg-stats.html",
+        "teaser": null
+      },{
+        "title": "Exploring Quest Design in *Hearthstone Battlegrounds* Season 2",
+        "excerpt":"Release Date: 30 Aug 2022 Developer: Blizzard Entertainment Publisher: Activision Blizzard Success and Growth of Battlegrounds After almost 3 years in the running (including Beta access), Hearthstone Battlegrounds has become one of the game’s cornerstone modes, largely in part to it’s ease of entry. Unlike the game’s Standard mode, Battlegrounds...","categories": ["Video Games"],
+        "tags": ["strategy","collectible-card-game","ranked"],
+        "url": "/video%20games/2022/09/13/hearthstone-battlegrounds-s2.html",
+        "teaser": null
+      },{
+        "title": "*Air, Land, & Sea*: An Easy to Learn Game with Incredible Depth",
+        "excerpt":"Image courtesy of boardgamequest.com Release Date: 2019 Developer: Jon Perry Publisher: Arcane Wonders Overview Air, Land, &amp; Sea is a two-player WWII-themed game where each player is attempting to control two of the three theaters (Air, Land, or Sea) over a series of battles until they can declare victory! With...","categories": ["Board Games"],
+        "tags": ["strategy","bluffing","war"],
+        "url": "/board%20games/2022/09/22/air-land-sea.html",
+        "teaser": null
+      },{
+        "title": "Automating Deployments with BitBucket / GitHub Pipelines",
+        "excerpt":"At work, I’ve recently been splitting our many of our services into their own repo, migrating from a Monorepo-style architecture towards an eventual Multirepo-style future. Since my .dotfiles repo is a Mono, I wanted to take some time to highlight the differences between the types of repos, how to configure...","categories": ["Programming"],
+        "tags": ["automation","infrastructure","tools"],
+        "url": "/programming/2022/11/20/automating-deployments.html",
+        "teaser": null
+      },{
+        "title": "My Top Single-Player Video Game of each Year",
+        "excerpt":"Overview I was recently looking through my library and was surprised how many years had passed since some of my favorite games were released. Many people are always focused on new titles – which makes sense, as fresh technology and ideas will deliver new experiences, building off of the learned...","categories": ["Video Games"],
+        "tags": ["deck-builder","platformer","puzzle","roguelike","rpg","shooter","speed-running","story","visual-novel","walking-simulator"],
+        "url": "/video%20games/2023/01/01/my-top-single-player-games.html",
+        "teaser": null
+      },{
+        "title": "Exploring: The Level Design of *Neon White*",
+        "excerpt":"Release Date: 16 Jun 2022 (PC) Developer: Angel Matrix Publisher: Annapurna Interactive Overview That’s not a typo in the title. While the baseline routes through each of Neon White’s ninety-seven levels are challenging and zippy, they are designed to be both carefully explored and naturally discovered, with multiple speedrunning optimizations...","categories": ["Video Games"],
+        "tags": ["shooter","speed-running"],
+        "url": "/video%20games/2023/03/08/neon-white-level-walkthrough.html",
+        "teaser": null
+      },{
+        "title": "A Guide to Hosting Static Content via Jekyll & GitHub Pages",
+        "excerpt":"Overview It’s been over a year since I’ve migrated my personal site to Jekyll. I had originally considered using React, as a way to practice a widely used technology, but quickly learned that there wasn’t actually much reactivity that I needed from my site (and so, not much to practice...","categories": ["Programming"],
+        "tags": ["automation","jekyll","markdown","mermaid","liquid"],
+        "url": "/programming/2023/04/06/jekyll-pages.html",
+        "teaser": null
+      },{
+        "title": "A Deep Dive into API Design and Auto-Generation",
+        "excerpt":"Overview An Application Programming Interface (API) is a set of rules that enable different applications to communicate with one another. They often expose a list of operations and associated types that callers can use to interact with a system. This is much like the interface pattern in most programming languages....","categories": ["Programming"],
+        "tags": ["api","infrastructure","open-api","rust","swagger"],
+        "url": "/programming/2023/05/03/api-design.html",
+        "teaser": null
+      },{
+        "title": "Exploring the *Souls*-like Genre via *Death's Door* and *TUNIC*",
+        "excerpt":"  Death’s Door TUNIC Release Date: 20 Jul 2021 (PC) 16 Mar 2022 (PC) Developer: Acid Nerve Isometricorp Games Publisher: Devolver Digital Finji Overview I recently finished playing TUNIC, which reminded me a lot of Death’s Door, a game which I enjoyed but otherwise forgot about until considering how similar...","categories": ["Video Games"],
+        "tags": ["souls-like","story","puzzle"],
+        "url": "/video%20games/2023/05/07/comparing-deaths-door-and-tunic.html",
+        "teaser": null
+      },{
+        "title": "Using the ***Facade*** Pattern to Split Tightly-Coupled Services",
+        "excerpt":"As services grow, there are often tendencies to expand existing API surfaces, rather than breaking contexts apart. This results in more tightly-coupled services which should be broken apart as the bloated system matures into more distinct layers. Taking Advantage of Facades Let’s take, for example, a persisted StoreSummary entity which...","categories": ["Programming"],
+        "tags": ["api","architecture","design-patterns","facade"],
+        "url": "/programming/2023/08/30/facade-pattern.html",
+        "teaser": null
+      }]
